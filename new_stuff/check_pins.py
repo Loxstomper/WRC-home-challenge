1b9a5c983ab0aa07040c94c5f340093a485b3a23
Lochie <lochieashcroft@gmail.com>
2018-08-05 08:46:34 +0000

    added sleep

--- new_stuff/check_pins.py
+++ new_stuff/check_pins.py
@@ -1,6 +1,7 @@
 # sets all the motor outputs to high
 
 import RPi.GPIO as GPIO
+from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -9,6 +10,9 @@ pins = [31, 33, 35, 37, 24, 26]
 for pin in pins:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.HIGH)
+
+sleep(5)
+
 
 GPIO.cleanup()
 
