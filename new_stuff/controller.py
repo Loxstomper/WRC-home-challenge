95ca0ac0b6ee533db95b2614b276c67d7d451b25
lox <lox.ash@hotmail.com>
2018-08-05 18:17:58 +1000

    Created robot class, Wheels class, and made the controller interface with the Robot class

--- new_stuff/controller.py
+++ new_stuff/controller.py
@@ -1,32 +1,50 @@
 import Motor
+import Wheels
+import Robot
 import threading
 
 
 class Control_Thread(threading.Thread):
-    def __init__(self, m):
+    def __init__(self, robot):
         threading.Thread.__init__(self)
-        self.motor = m
+        self.robot = robot
 
     def run(self):
-        y = int(input("Speed: "))
+        print("DIR = {F, R, L, R}")
 
         while True:
-            self.motor.set_speed(y)
-            y = int(input("Speed: "))
+            z = input("DIR SPEED TIME").split()
+
+            if z[0] == 'F':
+                self.robot.move_forward(int(z[1]), int(z[2]))
+            if z[0] == 'R':
+                self.robot.move_backward(int(z[1]), int(z[2]))
+            if z[0] == 'L':
+                self.robot.turn_left(int(z[1]), int(z[2]))
+            if z[0] == 'R':
+                self.robot.turn_right(int(z[1]), int(z[2]))
 
 
+left_motor = Motor.Motor(31, 33, 24)
+left_motor_t = Motor.Motor_Thread(m1)
 
-m1 = Motor.Motor(31, 33, 24)
-m1_t = Motor.Motor_Thread(m1)
+right_motor = Motor.Motor(35, 37, 26)
+right_motor_t = Motor.Motor_Thread(m2)
 
-m1.b_enabled = True
+wheels = Wheels.Wheels(left_motor, right_motor)
+
+
+robot = Robot.Robot(wheels)
 
 c_t = Control_Thread(m1)
 
+# start threads
 c_t.start()
-m1_t.start()
+left_motor_t.start()
+right_motor_t.start()
+
+# join the control thread
 c_t.join()
-#m1_t.join()
 
 
 
