37f4d4dd41ed8e7434c28a1668337f6031e48e2e
loxstomper <lochieashcroft@gmail.com>
2018-08-20 20:04:03 +1000

    Created a vision module

---
+++ new_stuff/Vision.py
@@ -0,0 +1,20 @@
+'''
+module for vision
+'''
+
+
+class Vision():
+    def __init__(self):
+        pass
+
+    def get_position(self):
+        ''' treating 0 as the centre of the screen
+            will have some thresholds
+            called will be able to move to center it
+            '''
+        pass
+
+class Visition_Thread():
+    def __init__(self):
+        pass
+
