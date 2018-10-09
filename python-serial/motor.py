9c5936b51505d2008c688a816de0ef79e3630bba
jaydenThomson1999 <jayden.thomson@griffithuni.edu.au>
2018-10-09 10:39:22 +1000

    made changes

--- python-serial/motor.py
+++ python-serial/motor.py
@@ -1,6 +1,8 @@
 import serial
 from time import sleep
 
+motorLst = ["left", "right", "claw", "first", "second"]
+
 # new
 def stop():
     message = "SET:M:{0}:0:0:{1}"
@@ -49,16 +51,26 @@ def bend_elbow(speed):
     ser.write((message.format("second", speed)).encode())
 
 
-def raise_arm(speed):
-    message = "SET:M:{0}:1:0:{1}"
+def raise_arm():
+    messageStart = "SET:M:{0}:1:0:{1}"
+    messageStop = "SET:M:{0}:0:0:{1}"
+    ser.write((messageStart.format("first", 200)).encode())
+    sleep(2)
+    ser.write((messageStop.format("first", 0)).encode())
 
-    ser.write((message.format("first", speed)).encode())
 
 
 def drop_arm(speed):
-    message = "SET:M:{0}:1:0:{1}"
+    message = "SET:M:{0}:0:1:{1}"
 
     ser.write((message.format("first", speed)).encode())
+
+
+def crash():
+    message = "SET:M:{0}:0:0:{1}"
+    for motor in motorLst:
+        ser.write((message.format(motor, 0)).encode())
+
 
 if __name__ == "__main__":
     ser = serial.Serial('/dev/ttyACM0', timeout=1)
@@ -68,4 +80,5 @@ if __name__ == "__main__":
         drop_arm(200)
         sleep(5)
 
+        crash()
 
